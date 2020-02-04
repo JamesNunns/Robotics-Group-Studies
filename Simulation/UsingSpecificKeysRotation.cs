6691b1ab0e38e11c9058716067daa821c1bd8dd0
JamesNunns <jcn767@student.bham.ac.uk>
2020-02-04 21:34:31 +0000

    No really I fixed the c# asset this time

--- Simulation/UsingSpecificKeysRotation.cs
+++ Simulation/UsingSpecificKeysRotation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour
+public class UsingSpecificKeysRotation : MonoBehaviour
 {
     public float Torque;
     public KeyCode ClockwiseKey;
