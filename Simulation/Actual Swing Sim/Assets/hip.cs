897a158c8de15f02c40f7bff95920069f2452b9a
JamesNunns <jcn767@student.bham.ac.uk>
2020-02-04 15:51:03 +0000

    Unity simulation added

---
+++ Simulation/Actual Swing Sim/Assets/hip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hip : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody2D rb2d;
+
+	private HingeJoint2D hinge;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		hinge = GetComponent<HingeJoint2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		if (Input.GetKey("q"))
+		{
+			rb2d.AddTorque(-500);
+		}
+		if (Input.GetKey("e"))
+		{
+			rb2d.AddTorque(500);
+		}
+		hinge.useLimits = true;
+	}
+}
