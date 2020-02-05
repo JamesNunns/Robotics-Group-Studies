3665e23bce3ead807d59675a7058414f625c029e
JamesNunns <jcn767@student.bham.ac.uk>
2020-02-05 12:53:04 +0000

    Added a function to lock all joints

--- Simulation/James Swing Sim/Assets/Knee.cs
+++ Simulation/James Swing Sim/Assets/Knee.cs
@@ -4,29 +4,66 @@ using UnityEngine;
 
 public class Knee : MonoBehaviour
 {
-	public float speed;
 
 	private Rigidbody2D rb2d;
 
 	private HingeJoint2D hinge;
 
+	private Rigidbody2D leg;
+
+	private JointMotor2D motor;
+
 	void Start()
 	{
+		leg = GameObject.Find("Leg").GetComponent<Rigidbody2D>();
 		rb2d = GetComponent<Rigidbody2D> ();
 		hinge = GetComponent<HingeJoint2D> ();
+		motor = hinge.motor;
+	}
+
+	public void NewAngle(float ang)
+	{ 
+		leg.freezeRotation = false;
+		if (hinge.jointAngle > ang)
+		{
+			rb2d.AddTorque(500);
+		}
+		if (hinge.jointAngle <= ang)
+		{
+			leg.freezeRotation = true;
+		}
+	}
+
+	public void NewAngle1(float ang)
+	{ 
+		leg.freezeRotation = false;
+		if (hinge.jointAngle < ang)
+		{
+			rb2d.AddTorque(-500);
+		}
+		if (hinge.jointAngle >= ang)
+		{
+			leg.freezeRotation = true;
+		}
 	}
 
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
 		if (Input.GetKey("a"))
 		{
-			rb2d.AddTorque(-500);
+			rb2d.AddTorque(-50);
 		}
 		if (Input.GetKey("d"))
 		{
-			rb2d.AddTorque(500);
+			rb2d.AddTorque(50);
 		}
-		hinge.useLimits = true;
+		if (Input.GetKey("k"))
+		{
+			NewAngle(200);
+		}
+		if (Input.GetKey("l"))
+		{
+			NewAngle1(330);
+		}
 	}
 }
