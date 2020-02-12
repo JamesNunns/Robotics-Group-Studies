38a12c99c39233ba0053f0bb0cec1e2ad88ed068
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2020-02-12 12:22:31 +0000

    SwingSpawning2D (#39)
    Modified the 2D swinging simulation to spawn a desired number of entities in a line.

---
+++ Simulation/James Swing Sim/Assets/SwingConstructors/SpawnSwing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSwing : MonoBehaviour
+{
+    public GameObject spawner;
+    public GameObject swing;
+    public Transform position;
+    public float swingSep;
+    public int swingToMake;
+    // Start is called before the first frame update
+    void Start()
+    {
+        makeSwing(swingToMake);
+    }
+
+    void makeSwing(int swingsToMake)
+    {
+        spawner.transform.Translate(Vector3.left * swingSep * (int)(swingToMake / 2));
+        for (int i = 0; i < swingsToMake; i++)
+        {
+            Instantiate(swing, position.position, position.rotation);
+            spawner.transform.Translate(Vector3.right * swingSep);
+        }
+    }
+}
