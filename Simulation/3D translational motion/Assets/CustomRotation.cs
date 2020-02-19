0405f877ff0cb62cbc94a22bdc0a863397b5b1da
JamesNunns <60348366+JamesNunns@users.noreply.github.com>
2020-02-19 14:52:01 +0000

    Documented CustomRotation.cs and edited Unity limits (#62)

--- Simulation/3D translational motion/Assets/CustomRotation.cs
+++ Simulation/3D translational motion/Assets/CustomRotation.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomRotation : MonoBehaviour
+/// <summary>
+/// This class controls the movement of the simulation
+/// and attempts to replicate Nao's interface closely.
+///<\summary>
 {
+	/// Define all the joints that can move
 	HingeJoint leftShoulder;
 	HingeJoint rightShoulder;
 
@@ -21,7 +26,7 @@ public class CustomRotation : MonoBehaviour
 
 	HingeJoint Neck;
 
-
+	/// Have a limiting angle for each joint
 	float leftShoulderAngle;
 	float rightShoulderAngle;
 
@@ -39,16 +44,19 @@ public class CustomRotation : MonoBehaviour
 
 	float neckAngle;
 	
+	/// Pair up the joints for symmetrical movement
 	List<HingeJoint> Shoulders = new List<HingeJoint>();
 	List<HingeJoint> Elbows = new List<HingeJoint>();
 	List<HingeJoint> Knees = new List<HingeJoint>();
 	List<HingeJoint> Ankles = new List<HingeJoint>();
 	List<HingeJoint> Hips = new List<HingeJoint>();
+	/// List every joint and angle together
 	List<HingeJoint> allJoints = new List<HingeJoint>();
 	List<float> allAngles = new List<float>();
 
 	void Start()
 	{
+		/// Assign all of the defined joints to the correct game objects
 		leftShoulder = GameObject.Find("LeftBicep").GetComponent<HingeJoint>();
 		rightShoulder = GameObject.Find("RightBicep").GetComponent<HingeJoint>();
 
@@ -66,6 +74,7 @@ public class CustomRotation : MonoBehaviour
 
 		Neck = GameObject.Find("Body").GetComponent<HingeJoint>();
 
+		/// Set all the starting angles to 0
 		leftShoulderAngle = 0;
 		rightShoulderAngle = 0;
 		leftElbowAngle = 0;
@@ -78,6 +87,7 @@ public class CustomRotation : MonoBehaviour
 		rightHipAngle = 0;
 		neckAngle = 0;
 
+		/// Populate the lists with the correct HingeJoint objects
 		Shoulders.Add(leftShoulder);
 		Shoulders.Add(rightShoulder);
 
@@ -110,7 +120,7 @@ public class CustomRotation : MonoBehaviour
 		allAngles.Add(leftElbowAngle);
 		allAngles.Add(rightElbowAngle);
 		allAngles.Add(leftKneeAngle);
-		allAngles.Add(rightElbowAngle);
+		allAngles.Add(rightKneeAngle);
 		allAngles.Add(leftAnkleAngle);
 		allAngles.Add(rightAnkleAngle);
 		allAngles.Add(leftHipAngle);
@@ -120,8 +130,18 @@ public class CustomRotation : MonoBehaviour
 	}
 
 	void Move(HingeJoint joint, float ang, float speed)
+	/// <summary>
+	/// This class moves a single joint to the passed angle
+	/// at a specified speed. The angles are defined in the
+	/// same way as in the Nao interface. Speed will also be,
+	/// but currently is not.
+	/// <\summary>
 	{   
+		/// direction sets motion to be clockwise or anti
 		float direction = 1;
+
+		/// Some angles have to be changed because of how
+		/// Nao defines its angles
 		if (Shoulders.Contains(joint))
 		{
 			ang += 90;
@@ -140,20 +160,23 @@ public class CustomRotation : MonoBehaviour
 			ang += 90;
 		}
 		for (int i = 0; i < 11; i++)
+		/// Set the angle in the list allAngles to the correct limit angle
 		{
 			if (joint == allJoints[i])
 			{
 				allAngles[i] = ang;
 			}
 		}	
+		/// Turn off joint limits temporarily
 		joint.useLimits = false;
+		/// Define a motor for the joint
 		JointMotor motor = joint.motor;
-		
 		if (ang < joint.angle + 90)
+		/// Determine direction of rotation
 			{
 				direction = -1;
 			}
-		
+		/// Set the motor velocity, and switch the motor on
 		motor.targetVelocity = speed*direction;
 		joint.motor = motor;
 		joint.useMotor = true;
@@ -161,18 +184,26 @@ public class CustomRotation : MonoBehaviour
 	}
 
 	void MoveSymmetric(List<HingeJoint> joints, float ang, float speed)
+	/// <summary>
+	/// This method will move both joints together if they are a pair
+	/// <\summary>
 	{
 		Move(joints[0], ang, speed);
 		Move(joints[1], ang, speed);
 	}
 
 	void Lock(HingeJoint joint)
+	/// <summary>
+	/// This method will lock the joint so it cannot rotate more than 1 degree each way
+	/// <\summary>
 	{
+		/// Define the joint's motor and set its velocity to zero, then switch it on
 		JointMotor motor = joint.motor;
 		motor.targetVelocity = 0;
 		joint.motor = motor;
 		joint.useMotor = true;
 
+		/// Find the corresponding angle of the joints and set its limmits to +-1 of that angle
 		int ind = allJoints.IndexOf(joint);
 		JointLimits limits = joint.limits;
 		limits.min = allAngles[ind] -91;
@@ -183,6 +214,7 @@ public class CustomRotation : MonoBehaviour
 
 	void Update()
 	{
+		/// These are test inputs, they can be changed
 		if (Input.GetKey("m"))
 		{
 			MoveSymmetric(Hips, -50, 100);
@@ -196,13 +228,13 @@ public class CustomRotation : MonoBehaviour
 			MoveSymmetric(Hips, -50, 100);
 		}
 		for (int i = 0; i < 11; i++)
+		/// This will lock any joint that is within 1 degree of its limit angle
 		{
 			if (allAngles[i] < allJoints[i].angle + 91 && allAngles[i] > allJoints[i].angle + 89)
 			{
 				Lock(allJoints[i]);
 			}
 		}
-		print((allAngles[8], allJoints[8].angle + 90));
 
 	}
 }
