16ceeb366499458606ffa52e9005f65b46071a8e
Sanders109 <30392624+Sanders109@users.noreply.github.com>
2020-02-25 15:04:29 +0000

    new Algorithm (#78)

---
+++ Robotics/single_pendulum/single_angluar_vel.py
@@ -0,0 +1,23 @@
+class Velocity():
+
+    def __init__(self,values,all_data,**kwargs):
+
+         self.period = kwargs.get('period', 0.005)
+
+        self.start_time = values['time']
+        self.previous_time = values['time']
+        self.previous_be = values['be']
+
+        self.duration = kwargs.get('duration', float('inf'))
+
+        self.previous_vel = values['av']
+
+    def algo(self,values,all_data):
+        print values['time'], values['be']
+        if values['time'] < self.duration:
+            if values['av'] < 0:
+                return 'crunched'
+            elif values['av'] > 0:
+                return 'extended'
+        else:
+            return 'switch'
