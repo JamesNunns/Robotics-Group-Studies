3e9010a71763967be49747f3c8b957889ebee4ac
Henry Shaw <Shawly-not@users.noreply.github.com>
2020-02-28 10:14:08 +0000

    Update ml.py
    Added documentation

--- Machine Learning/ml.py
+++ Machine Learning/ml.py
@@ -2,17 +2,40 @@
 """
 Created on Thu Feb 27 15:33:07 2020
 
-@author: User
+@author: Henry S
 """
 
-from keras.models import load_models 
+from keras.models import load_models
 import numpy as np
 
-model = load_models('net.h5')
+MODEL = load_models('net.h5')
 
 def get_action(state):
-    
+    '''
+    Takes the current state (angle and angular velocity deg/sec) and returns
+    an integer depending on the calculated optimal move.
+
+    0 - leg out
+    1 - leg in
+    2 - torso out
+    3 - torso in
+    4 - nothing
+
+    Parameters
+    ----------
+    state : Array
+        the current angle and angular velocity of the rod
+
+    Returns
+    -------
+    action : int
+        optimal move
+    '''
+
     if state is None:
+    # If there is no current state (i.e. at the start) makes a random move
         return np.random.randint(0, high=4)
-    action = np.argmax(model.predict(state.reshape(-1, len(state)))[0])
+
+    # the optimal move given the state
+    action = np.argmax(MODEL.predict(state.reshape(-1, len(state)))[0])
     return action
