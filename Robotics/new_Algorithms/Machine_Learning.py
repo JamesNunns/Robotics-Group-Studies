7138a239a45a92aef1a7a0a35570b07eac580096
WPetchey <56683250+WPetchey@users.noreply.github.com>
2020-03-02 15:12:30 +0000

    Added files to test and interface Machine Learning with Nao (#91)

--- Robotics/new_Algorithms/Machine_Learning.py
+++ Robotics/new_Algorithms/Machine_Learning.py
@@ -2,11 +2,8 @@ from robot_interface import Robot
 from encoder_interface import Encoders
 from sys import path
 path.insert(0, 'single_pendulum')
-from single_test_brute import Test
-from single_damping_small_angles import SmallAngleDamping
-from single_startup_const_period import Start
-from single_angluar_vel import Velocity
-
+from machine
+from single_machine_learning import Machine_Learning
 from single_nothing import Nothing
 
 class Algorithm(Robot, Encoders):
@@ -14,16 +11,7 @@ class Algorithm(Robot, Encoders):
     def __init__(self, BigEncoder, SmallEncoders, values, positions, ALProxy, period):
 
         Encoders.__init__(self, BigEncoder, SmallEncoders, small_encoders_required=False)
-
         Robot.__init__(self, values, positions, ALProxy, masses=False, acc_required=False, gyro_required=False)
 
-        self.order = [
-        {
-			'algo': Nothing,
-			'duration': 20
-        },
-        {
-            'algo': Velocity,
-            'duration': 30
-        }
-        ]
+        self.order = [{'algo':Nothing, 'duration':10}, {'algo':Machine_Learning, 'duration': 600}]
+        
