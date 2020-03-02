d622c1410a7bb3ff0e52695c35523a9d5893c4de
Robert Noakes <56394369+rxn742@users.noreply.github.com>
2020-03-02 15:46:57 +0000

    Fixed ML-movement, tested working with keyboard input in webots (#93)

--- Robotics/ML_movement_test.py
+++ Robotics/ML_movement_test.py
@@ -1,17 +1,17 @@
-from robot_interface import Robot
+from Webots_interface import Robot
 from naoqi import ALProxy
 from positions_new import positions
 from limb_data_2020 import values
 
 
-Robot = Robot(values, positions, ALProxy, ip="127.0.0.1", 9559)
+Robot = Robot(values, positions, ALProxy, "127.0.0.1", 9559)
 
 
 def move_torso(angle, percent_max_speed):
     0.587522
-    torso = {'RHP': 1.0, 'LHP': 1.0, 'RSP': 0.480417754569. 'LSP': 0.0496083550914, 'RSR': 1.12532637076, 'LSR': -1.10966057441, 'RER': -2.13838120104, 'LER': 2.18263145891, 'REY': -0.258485639687, 'LEY': 0.853785900783, 'RWY': 0.167101827676, 'LWY': -0.180156657963}
-    for joint, ratio in torso:
-        Robot.move_limbs(joint, angle*ratio, percent_max_speed)
+    torso = {'RHP': 1.0, 'LHP': 1.0, 'RSP': 0.480417754569, 'LSP': 0.0496083550914, 'RSR': 1.12532637076, 'LSR': -1.10966057441, 'RER': -2.13838120104, 'LER': 2.18263145891, 'REY': -0.258485639687, 'LEY': 0.853785900783, 'RWY': 0.167101827676, 'LWY': -0.180156657963}
+    for joint in torso:
+        Robot.move_limbs(joint, angle*torso[joint], percent_max_speed)
 
 
 def move_legs(angle, percent_max_speed):
@@ -21,17 +21,17 @@ def move_legs(angle, percent_max_speed):
 
 while True:
     key = raw_input("q = torso out\tw = torso in\to = legs in\tp = legs out\n")
-    if key == q:
+    if key == "q":
         move_torso(0.0174533, 0.4)
         print "Torso Out\n"
-    elif key == w:
+    elif key == "w":
         move_torso(-0.0174533, 0.4)
         print "Torso In\n"
-    elif key == o:
+    elif key == "o":
         move_legs(0.0174533, 0.4)
         print "Legs In\n"
-    elif key == p:
-        move_legs(0.0174533, 0.4)
+    elif key == "p":
+        move_legs(-0.0174533, 0.4)
         print "Legs Out\n"
     else:
         print "Unrecognised Key"
--- Robotics/Webots_interface.py
+++ Robotics/Webots_interface.py
@@ -14,7 +14,7 @@ class Robot():
     """
 
     def __init__(self, values, positions, ALProxy,
-                 ip="127.0.0.1", port=9559, **kwargs):
+                 ip="192.168.1.3", port=9559, **kwargs):
         """
         Sets up the connection to the robot and sets initial posture. Also calibrates encoders to zero, if available.
         Requires arguments:
@@ -36,12 +36,15 @@ class Robot():
         self.acc_required = kwargs.get('acc_required', False)
         self.gyro_required = kwargs.get('gyro_required', False)
 
+        accX_0, accY_0, accZ_0 = 0, 0, 0
+        self.calibrate_acc()
+
         self.set_posture_initial('crunched', max_speed = 0.1)
         self.motion.setMotionConfig( [["ENABLE_DISACTIVATION_OF_FALL_MANAGER", True]] )
         self.motion.setFallManagerEnabled(False)
         #self.speech.say('Battery level at {:.0f}%'.format(self.get_angle('BC')[0]*100))
         print 'Battery level at {:.0f}%'.format(self.get_angle('BC')[0]*100)
-        
+
     def check_setup(self, position):
         """
         Checks position values received from Nao match the position values it's meant to have
@@ -57,14 +60,14 @@ class Robot():
         differences = [(key, value, abs(value - position[key])) for (key, value) in zip(position.keys(), values)]
 
         incorrect_positions = [i for i in differences if i[2] > 0.5]
-        if len(incorrect_positions) != 0:        
+        if len(incorrect_positions) != 0:
             # for values in incorrect_positions:
                 # print values[0], 'Actual value: {}'.format(values[1]), 'Difference from expected: {}'.format(values[2])
-            
+
             error_amounts = ['{}, Actual value: {}, Difference from expected: {}'.format(*v) for v in incorrect_positions]
             raise PositionError("Initial check of setup failed\n" + '\n'.join(error_amounts))
-            
-            
+
+
     def get_gyro(self):
         """
         Obtain the current gyroscope data. Returns a tuple containing the (x, y, z) gyroscope data,
@@ -87,14 +90,14 @@ class Robot():
         else:
             return [0.0, 0.0, 0.0]
 
-    def get_acc(self):
+    def get_acc(self, plane):
         """
         Obtain the current accelerometer data. Returns a list containing the (x, y, z) acceleromenter data,
         in m/s.
         Args:
-            None
+            name of the undesired accelerometer
         Returns:
-            list containing x, y, and z acceleration
+            list containing x, y, and z acceleration (0.0 for undesired accelerometers)
         Example:
             > self.get_acc()
             [0.0, 1.1, 0.5]
@@ -102,9 +105,18 @@ class Robot():
         if self.acc_required: # This is slow and limits interface heavily
             # same again, not sure if this works but would be good to save time
             # x_data, y_data, z_data = self.memory.getData([self.values['ACX'][1], self.values['ACY'][1], self.values['ACZ'][1]])
-            x_data = self.memory.getData(self.values['ACX'][1])
-            y_data = self.memory.getData(self.values['ACY'][1])
-            z_data = self.memory.getData(self.values['ACZ'][1])
+            if plane == 'z':
+                x_data = self.memory.getData(self.values['ACX'][1]) - accX_0
+                y_data = self.memory.getData(self.values['ACY'][1]) - accY_0
+                z_data = 0.0
+            elif plane == 'x':
+                x_data = 0.0
+                y_data = self.memory.getData(self.values['ACY'][1]) - accY_0
+                z_data = self.memory.getData(self.values['ACZ'][1]) - accZ_0
+            elif plane == 'y':
+                x_data = self.memory.getData(self.values['ACX'][1]) - accX_0
+                y_data = 0.0
+                z_data = self.memory.getData(self.values['ACZ'][1]) - accZ_0
             return [x_data, y_data, z_data]
         else:
             return [0.0, 0.0, 0.0]
@@ -124,7 +136,31 @@ class Robot():
         angle = self.memory.getData(limb_info[1])
         name = limb_info[0]
         return angle, name
-        
+
+    def move_limbs(self, limb_name, angle, percent_max_speed):
+        """
+        Moves limbs by given angles
+        Args:
+            limb_name: string
+                shorthand key of the desired limb as given in the limb_data dictionary
+            angle: float
+                the angle to move the desired limb in radians
+            percent_max_speed: float
+                percentage of max speed given as a decimal between 0 and 1
+        Returns:
+            None
+        """
+        current_angle, long_name = self.get_angle(limb_name)
+        if current_angle + angle <= self.positions['maxs'][limb_name] and current_angle + angle >= self.positions['mins'][limb_name]:
+            self.motion.changeAngles(long_name, angle, percent_max_speed)
+        elif current_angle + angle > self.positions['maxs'][limb_name]:
+            self.motion.changeAngles(long_name, self.positions['maxs'][limb_name] - current_angle, percent_max_speed)
+        elif current_angle + angle < self.positions['mins'][limb_name]:
+            self.motion.changeAngles(long_name, self.positions['mins'][limb_name] - current_angle, percent_max_speed)
+
+
+
+
     def set_posture(self, next_posture, current_posture, max_speed=1.0):
         """
         Changes position from current_posture to next_posture, calculates correct speeds
@@ -141,7 +177,7 @@ class Robot():
         # Extract dictionaries corresponding to both positions
         next_posture_dict = self.positions[next_posture]
         current_posture_dict = self.positions[current_posture]
-        
+
         differences_in_angles = []
         for name in next_posture_dict.keys():
             # Calculate difference in angle for each joint
@@ -154,18 +190,18 @@ class Robot():
 
         # Normalise speeds to longest time
         max_difference = max(differences_in_angles)
-        speeds = [max_speed * diff / max_difference for diff in differences_in_angles]
-            
+        speeds = [max_speed * difference / max_difference for difference in differences_in_angles]
+
         # Extract name naoqi uses to set positions
         part_name = [self.values[name][0] for name in next_posture_dict.keys()]
-        
+
         # Change position
         for name, value, speed in zip(part_name, next_posture_dict.values(), speeds):
             self.motion.setAngles(name, value, speed)
         # Update self.position with now current position
         self.position = next_posture
-        
-    def set_posture_initial(self, next_posture='seated', max_speed=0.2):
+
+    def set_posture_initial(self, next_posture='crunched', max_speed=0.2):
         """
         Moves nao from whatever position he is currently in to a specified starting position, important
         this is used as otherwise speeds aren't normalised and that will destroy his joints
@@ -179,16 +215,47 @@ class Robot():
         """
         # This sets the stiffness permanently
         self.motion.setStiffnesses("Body", 1.0)
-        
+
         # Calculate his current position
         startup_dict = {}
         for key in self.positions[next_posture].keys():
             startup_dict[key] = self.get_angle(key)[0]
         # Add his current position to the positions dictionary
         self.positions['startup'] = startup_dict
-        
+
         # Switch from current position just added to next_posture
         self.set_posture(next_posture, 'startup', max_speed=max_speed)
-        
-        
-        
+
+    def get_posture(self):
+        '''
+        Returns the posture of NAO as a dictionary in the same format as the positions dictionary
+
+        Parameters
+        ------
+        summary: list
+            Raw list of current posture generated using "motionProxy.getSummary"
+
+        Returns
+        ------
+        current_posture: dict.
+            Dictionary of Nao limb data defining the current posture of Nao
+        '''
+        current_posture = {}
+        summary = self.motion.getSummary()
+        summary = summary.split()
+        summary = summary[7:-14]
+        for i in range(len(summary)/4):
+            for keys in values:
+                if values[keys][0] == summary[4*i]:
+                    current_posture[keys] = summary[4*i + 3]
+        return current_posture
+
+    def calibrate_acc(self):
+        '''
+        A function that takes no arguments and calibrates the accelerometers to zero
+        '''
+        global accX_0, accY_0, accZ_0
+        if self.acc_required:
+            accX_0, accY_0, accZ_0 = self.memory.getData(self.values['ACX'][1]), self.memory.getData(self.values['ACY'][1]), self.memory.getData(self.values['ACZ'][1])
+        else:
+            accX_0, accY_0, accZ_0 = 0, 0, 0
