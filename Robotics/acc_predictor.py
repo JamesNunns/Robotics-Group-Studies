2ca9f9c387961738ccf4b0590f286e42a740d72e
rxn742 <rxn742@student.bham.ac.uk>
2020-03-10 11:27:00 +0000

    cleaned up accelerometer algorithm

--- Robotics/acc_predictor.py
+++ Robotics/acc_predictor.py
@@ -14,17 +14,17 @@ def convert(fft, freq):
     fft_conv = np.zeros(fft.size)
     for i in xrange(len(fft)):
         if fft[i] != 0.0:
-            where = np.where(freq == min(freq, key=lambda x:abs(x-freq[np.argmax(fft)]/2)))[0][0]
+            absolutes = np.abs(freq-freq[i]/2)
+            where = np.argmin(absolutes)
             fft_conv[where] = fft[i]
             fft_conv[len(fft)-where] = np.conj(fft[i])
             break
     return fft_conv 
 
 def acc_predict(all_data, tl=0.77, th=0.82):
-    time.sleep(2)
-    Time = np.array(all_data['time'])[-128:]
+    Time = np.array(all_data['time'])
     rel_Time = Time - Time[0]
-    AccZ = np.array(all_data['az'])[-128:]
+    AccZ = np.array(all_data['az'])
     Tlist = []
     for i in xrange(len(rel_Time)-1):
         Tlist.append(rel_Time[i+1] - rel_Time[i])
@@ -35,15 +35,5 @@ def acc_predict(all_data, tl=0.77, th=0.82):
     fftz = filt(fftz, freqsz, tl, th)
     fft_conv = convert(fftz, freqsz)
     data = np.real(np.fft.ifft(fft_conv))
-    peaks = find_peaks(data)[0]
-    tp = []
-    for i in xrange(len(rel_Time[peaks])-1):
-        tp.append(rel_Time[peaks][i+1]-rel_Time[peaks][i])
-    tp = np.mean(tp)
-    last_maxima = rel_Time[peaks][-1]+Time[0]
-    next_2_maxima = []
-    next_2_minima = []
-    for i in xrange(5):
-        next_2_maxima.append(last_maxima+(i+1)*tp)
-        next_2_minima.append(last_maxima+(i+1)*tp - tp/2)
-    return next_2_maxima, next_2_minima
+    dic = {"time":Time, "be":data}
+    return dic
--- Robotics/single_pendulum/acc_predict.py
+++ Robotics/single_pendulum/acc_predict.py
@@ -1,6 +1,7 @@
 from sys import path
 path.insert(0, "../")
 from acc_predictor import acc_predict
+from utility_functions_new import last_key_point, next_points
 
 class Acc():
 
@@ -8,18 +9,21 @@ class Acc():
         self.period = kwargs.get('period', 0.01)
         self.start_time = values['time']
         self.duration = kwargs.get('duration', float('inf'))
+        self.offset = 0.25
 
     def algo(self,values,all_data):
         print values['time'], values['be'], values['ax'], values['az']
         if values['time'] - self.start_time < self.duration:
-            offset = 0.25
             p = acc_predict(all_data)
-            for i in p[0]:
-                if abs(values['time'] - i) < offset:
-                    return 'extended'
-            for i in p[1]:
-                if abs(values['time'] - i) < offset:
-                    return 'crunched'
-		else:
-			return 'switch'
+            l = [last_key_point("max", p), last_key_point("min", p), last_key_point("zero", p)]
+            next_max, next_min = next_points(values, l)
+            if next_max - values['time'] < self.offset:
+                if next_max - values['time'] > 0:
+                    return "extended"
+            if next_min - values['time'] < self.offset:
+                if next_min - values['time'] > 0:
+                    return "crunched"
+        else:
+            print "done"
+            return 'switch'
         
