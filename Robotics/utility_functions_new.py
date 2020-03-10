86b27ce36349570b5fbac5ca82c52d589b479a59
Robert Noakes <56394369+rxn742@users.noreply.github.com>
2020-03-10 10:08:13 +0000

    Cleaned up new algorithm and plot functions (#132)

--- Robotics/utility_functions_new.py
+++ Robotics/utility_functions_new.py
@@ -32,5 +32,25 @@ def last_key_point(what, all_data):
                 min_time.append(tv[i] - interpolate)
         return min_time[-1]
     
-    
+def next_points(values, last_key_points):
+    l = last_key_points
+    if values['time'] - l[1] < values['time'] - l[2] and values['time'] - l[0]:
+        quarter_period = abs(l[1] - l[2])
+        next_max = l[1] + 2*quarter_period
+        next_min = l[1] + 4*quarter_period
+    if values['time'] - l[2] < values['time'] - l[1] and values['time'] - l[0]:
+        if values['time'] - l[1] < values['time'] - l[0]: 
+            quarter_period = abs(l[2] - l[1])
+            next_max = l[2] + quarter_period
+            next_min = l[2] + 3*quarter_period
+        if values['time'] - l[0] < values['time'] - l[1]: 
+            quarter_period = abs(l[2] - l[0])
+            next_max = l[2] + 3*quarter_period
+            next_min = l[2] + quarter_period
+    if values['time'] - l[0] < values['time'] - l[1] and values['time'] - l[2]:
+        quarter_period = abs(l[0] - l[2])
+        next_max = l[0] + 4*quarter_period
+        next_min = l[0] + 2*quarter_period
+        
+    return next_max, next_min
     
